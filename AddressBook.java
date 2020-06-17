f9d229933b6e08c7f03aaf5d46a2e6a13f1d2e47
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-17 09:56:33 -0700

    Added: ArrayList to store multiple contacts

--- AddressBook.java
+++ AddressBook.java
@@ -1,9 +1,34 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
     public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
         System.out.println("Welcome to Adderess Book program");
-        Person person = PersonUtils.record();
-        System.out.println(person);
+        boolean isContinue = true;
+        while (isContinue == true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("1)ADD   7)EXIT");
+            int select = sc.nextInt();
+            switch (select) {
+                case 1:
+                    Person person = PersonUtils.record();
+                    //System.out.println(person);
+                    personList.add(person);
+                    //System.out.println(personList.size());
+                    break;
+                case 7:
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("invalid input");
+                    break;
+            }
+
+
+        }
     }
 }
